25eb47a1cf3564abfeea7fc930d5ef980ccd8c63
Bruno Palhano <107354811+jbrunopg@users.noreply.github.com>
2022-11-23 12:39:24 -0300

    Update Conversor_Moeda.py

--- Conversor_Moeda.py
+++ Conversor_Moeda.py
@@ -1,7 +1,7 @@
-real = float(input('Quanto de dinheiro você tem na carteira? R$'))
+real = float(input('Quanto de dinheiro você tem na carteira?\n R$'))
 dolar = real / 5.35
 print('Com R$ {:.2f} você pode comprar US$ {:.2f}'.format(real, dolar))
 
-real = float(input('Quanto de dinheiro você tem na carteira? R$'))
+real = float(input('Quanto de dinheiro você tem na carteira?\n R$'))
 euro = real / 5.51
 print('Com R$ {:.2f} você pode comprar € {:.2f}'.format(real, euro))
